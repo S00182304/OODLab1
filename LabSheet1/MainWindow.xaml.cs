c14271bf424103d2264685f01685cb83e85e9975
Karen Harte <44403544+KarenRHarte@users.noreply.github.com>
2020-01-22 14:58:58 +0000

    Add Band details and add more features to the main window

--- LabSheet1/MainWindow.xaml.cs
+++ LabSheet1/MainWindow.xaml.cs
@@ -23,10 +23,28 @@ namespace LabSheet1
             InitializeComponent();
         }
 
-        private void LBBandDetails_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //Loading the band details
-            Band Band1 = new Band("Rolling Stones", 1962, "Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts, and Ian Stewart");
+            Band Band1 = new Band("Rolling Stones", 1962, "Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts and Ian Stewart");
+            Band Band2 = new Band("Led Zeppelin", 1968, "Robert Plant, Jimmy Page, John Paul Jones and John Bonham");
+            Band Band3 = new Band("The Beatles", 1957, "John Lennon, Paul McCartney, George Harrison and Ringo Starr");
+            Band Band4 = new Band("Pink Floyd", 1965, " Syd Barrett, David Gilmour, Roger Waters, Richard Wright, Nick Mason and Bob Klose");
+            Band Band5 = new Band("Metallica", 1981, "James Hetfield, Cliff Burton, Lars Ulrich, Dave Mustaine etc");
+            Band Band6 = new Band("Guns N' Roses", 1985, "Slash, Axl Rose, Duff McKagan, Izzy Stradlin etc");
+
+
+            //Add them to a list
+            List<Band> bandList = new List<Band>();
+            bandList.Add(Band1);
+            bandList.Add(Band2);
+            bandList.Add(Band3);
+            bandList.Add(Band4);
+            bandList.Add(Band5);
+            bandList.Add(Band6);
+
+            //Populate them into a listBox
+            LBBandDetails.ItemsSource = bandList;
 
         }
     }
