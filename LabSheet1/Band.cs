9cb1a3cda2514665d8125c2d0dd92ebfdb766b07
Karen Harte <44403544+KarenRHarte@users.noreply.github.com>
2020-01-22 14:08:55 +0000

    Add project files.

---
+++ LabSheet1/Band.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet1
+{
+    public class Band
+    {
+        public string BandName { get; set; }
+        public int BandYearFormed { get; set;}
+        public string BandMembers { get; set; }
+
+        public Band (string bandName, int bandYearFormed, string bandMembers)
+        {
+            BandName = bandName;
+            BandYearFormed = bandYearFormed;
+            BandMembers = bandMembers;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{BandName}, {BandYearFormed}, {BandMembers}");
+        }
+
+    }
+}
---
+++ LabSheet1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabSheet1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LBBandDetails_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Loading the band details
+            Band Band1 = new Band("Rolling Stones", 1962, "Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts, and Ian Stewart");
+
+        }
+    }
+}
