0574791bbb849e08a693ee838f83da4c82a300f6
Karen Harte <S00182304@mail.itsligo.ie>
2020-03-19 19:39:02 +0000

    Lab 5 Creating Databases Started

---
+++ LabSheet5CreateDb/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabSheet5CreateDb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BandsAlbumsContainer db = new BandsAlbumsContainer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from a in db.Bands
+                       select a;
+            dgBands.ItemsSource = query.ToList();
+        }
+    }
+}
