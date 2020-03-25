998a62bed70c9fb8d8010842325cb58e7c53a9b7
Karen Harte <S00182304@mail.itsligo.ie>
2020-03-25 21:08:39 +0000

    Lab Sheet 8 started

--- LabSheet8/MainWindow.xaml.cs
+++ LabSheet8/MainWindow.xaml.cs
@@ -13,31 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LabSheet5CreateDb
+namespace LabSheet8
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        BandsAlbumsContainer db = new BandsAlbumsContainer();
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Windows_Loaded(object sender, RoutedEventArgs e)
-        {
-            var query = from b in db.Bands
-                        select new
-                        {
-                           b.Name
-
-                       };
-
-            dgBands.ItemsSource = query.ToList();
-            
-
         }
     }
 }
