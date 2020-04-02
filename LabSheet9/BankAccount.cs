44d14da95f357bf06754de7c09f58c965ad3c3ff
Karen Harte <44403544+S00182304@users.noreply.github.com>
2020-04-02 12:32:06 +0100

    LabSheet9 Started

---
+++ LabSheet9/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LabSheet9
+{
+    public class BankAccount
+    {
+        public double Balance { get; set; }
+
+        public double DepositMoney(double Deposit)
+        {
+            Deposit += Balance;
+            return Balance;
+        }
+
+        public double WithdrawMoney(double Withdraw)
+        {
+            Withdraw -= Balance;
+            return Balance;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //double testAnswer = 10.5;
+
+            //Act
+            //var result = LabSheet9.BankAccount.DepositMoney()
+            //Assert
+
+        }
+    }
+}
